ab94ca659820c6429bbab1da3933e8eab7592cf4
SaimMeEr <147688531+SaimMeEr@users.noreply.github.com>
2023-10-12 11:57:23 +0000

    variables javascript in depth

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 141414
+let accountEmail="saim21@google.com"
+var accountPassword="1234"
+accountCity= "jaipur"
+let accountState;
+
+
+// accounrId=2  not allowed as constant can`t be changed
+accountEmail="mir312@google.com"
+accountPassword="3214"
+accountCity="Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
