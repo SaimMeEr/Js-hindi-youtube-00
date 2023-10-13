668fe59c30b6f4943b1a196f93a555ebb4b59b62
SaimMeEr <147688531+SaimMeEr@users.noreply.github.com>
2023-10-13 04:36:35 +0000

    Data types

---
+++ .devcontainer/01_basics/02_data types.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using node js not browser
+
+//console.log(3+
+3)     //code readibility shoud be high
+
+//Data types
+let name ="sameer"
+let age=22;
+let isLoggedIn=true
+
+// 1.number => range is 2 to the power 53
+// 2.bigint
+// 3.string => ""
+// 4.bolean => true/false
+// 5.null =>standalone value
+// 6.undefined =>
+// 7.symbol => uniqueness
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
