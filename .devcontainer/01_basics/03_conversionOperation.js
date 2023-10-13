1a893fb0e710ab5e825539c880189d9dc8bb2dc0
SaimMeEr <147688531+SaimMeEr@users.noreply.github.com>
2023-10-13 05:49:45 +0000

    conversion operations

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score=33
+
+// console.log(typeof score);
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NAN
+//true=>1 ; false=>0
+
+// let isloggedIn=1;
+
+// let booleanIsloggedIn=boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1=>true;0=>false
+// ""=>false;
+// "samerr"=>true
+
+let someNumber=33
+let stringNumber=string(someNumber)
+console.log(stringNumber);
