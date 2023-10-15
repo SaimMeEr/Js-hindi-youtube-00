950ce73ba0763348fb37818a9cf70616bb0fc05b
SaimMeEr <147688531+SaimMeEr@users.noreply.github.com>
2023-10-15 06:46:49 +0000

    dataTypes-summary js

---
+++ .devcontainer/01_basics/dataTypes-summary.js
@@ -0,0 +1,19 @@
+// primitive data types
+
+// 7-types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100 //number defined
+const IsloogedIn = false // bolean defined
+const outsideTemp = null  //null defined
+let useremail; //undefined defined
+
+//To declare symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+//To declare BigInt
+const bigNumber = 111412313163n
+
+
+//Refrence(Non-primitive) => Arrays, Objects, functions
