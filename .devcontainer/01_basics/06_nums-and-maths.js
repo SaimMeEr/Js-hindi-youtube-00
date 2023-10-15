ec0949ead3e4e6a319222ffc390cc20b0c06c8a9
SaimMeEr <147688531+SaimMeEr@users.noreply.github.com>
2023-10-15 12:32:15 +0000

    Nums and maths in js

---
+++ .devcontainer/01_basics/06_nums-and-maths.js
@@ -0,0 +1,26 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(400)
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toFixed(1))
+
+const otherNumber=123
+console.log(otherNumber.toPrecision(3))
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+//******************MATHS**************
+// console.log(maths)
+// console.log(Maths.abs(-4)) //=>gives the positive value
+// console.log(Maths.round(4.6)) //=> gives the nearest integer
+// console.log(Maths.ceil(4.2)) //=>gives the upper value
+// console.log(Maths.floor(4.9)) //=>gives the lowest value
+// console.log(Maths.min(1,2,3,4)) //=>gives the minimum number
+// console.log(Maths.max(4,3,21)) //=>gives the maximmum number
+
+console.log(Maths.random()) // =>gives the random value b/w 0 and 1 and vice-versa
